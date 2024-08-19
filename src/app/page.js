20ff64cdc838ddb69886d3e84306091afe4d57eb
Elihu-beep <158291777+Elihu-beep@users.noreply.github.com>
2024-08-19 16:13:59 +0300

    Updating tvPopular to upcoming movies

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Results from './components/Results'
 
 const urls = {
-  tvPopular: 'https://api.themoviedb.org/3/tv/popular?language=en-US&page=1',
+  upcoming: 'https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1',
   moviesPopular: 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc',
   highestRated: 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=vote_average.desc&without_genres=99,10755&vote_count.gte=200',
 }
@@ -17,38 +17,38 @@ export default async function page() {
   }
 
   try {
-    const [resPopularTv, resPopularMovies, resHighestRated] = await Promise.all([
-      fetch(urls.tvPopular, options),
+    const [resUpcoming, resPopularMovies, resHighestRated] = await Promise.all([
+      fetch(urls.upcoming, options),
       fetch(urls.moviesPopular, options),
       fetch(urls.highestRated, options)
     ])
 
-    if (!resPopularTv.ok || !resPopularMovies.ok || !resHighestRated.ok) {
+    if (!resUpcoming.ok || !resPopularMovies.ok || !resHighestRated.ok) {
       throw new Error('Error fetching data')
     }
 
-    const [popularTvData, popularMovieData, highestRatedData] = await Promise.all([
-      resPopularTv.json(),
+    const [upcomingMovieData, popularMovieData, highestRatedData] = await Promise.all([
+      resUpcoming.json(),
       resPopularMovies.json(),
       resHighestRated.json()
     ])
 
-    const tvResults = popularTvData.results
+    const upcomingResults = upcomingMovieData.results
     const movieResults = popularMovieData.results
     const highestRatedResults = highestRatedData.results
 
     return (
       <div className='p-8 max-w-5xl items-center mx-auto'>
         <div>
-          <span className='sm:text-xl text-base font-semibold text-red-500'>| </span><span className='sm:text-xl text-base font-medium'>CURRENTLY POPULAR TV</span>
-          <Results results={tvResults} />
+          <span className='sm:text-xl text-base font-semibold text-red-500'>| </span><span className='sm:text-xl text-base font-medium'>NEW AND UPCOMING</span>
+          <Results results={upcomingResults} />
         </div>
         <div className='py-10'>
-          <span className='sm:text-xl text-base font-semibold text-red-500'>| </span><span className='sm:text-xl text-base font-medium'>CURRENTLY POPULAR MOVIES</span>
+          <span className='sm:text-xl text-base font-semibold text-red-500'>| </span><span className='sm:text-xl text-base font-medium'>CURRENTLY POPULAR</span>
           <Results results={movieResults} />
         </div>
         <div className='py-10'>
-          <span className='sm:text-xl text-base font-semibold text-red-500'>| </span><span className='sm:text-xl text-base font-medium'>HIGHEST RATED MOVIES</span>
+          <span className='sm:text-xl text-base font-semibold text-red-500'>| </span><span className='sm:text-xl text-base font-medium'>HIGHEST RATED </span>
           <Results results={highestRatedResults} />
         </div>
       </div>
